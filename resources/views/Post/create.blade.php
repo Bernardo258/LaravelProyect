1c1ec5bcd8cd83253e234554810c171cab9d1c3c
Bernardo <bernaparedesdsns@gmail.com>
2023-02-22 18:53:19 -0600

    21_02_2023 commit

--- resources/views/Post/create.blade.php
+++ resources/views/Post/create.blade.php
@@ -1,10 +1,20 @@
 @extends('layout.layouts')
 
+@push('styles')
+<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
+
 @section('contenido')
 <div class="row">
     <div class="col-4 p-5 text-center m-2 rounded-2">
         <h4 class="mb-3">imagen aqui</h4>
-        <a class="btn btn-primary" href="" role="button">Call to action</a>
+        <!-- Example of a form that Dropzone can take over -->
+        <form  action="/post" enctype="multipart/form-data"
+        id="dropzone" class="dropzone rounded border border-1 w-100 h-100 items-center d-flex aling-items-center
+        justify-content-center">
+        @csrf
+        </form>
     </div>
  
     <div class="col-7 p-5 text-center m-2 rounded-2 border">
--- resources/views/layout/layouts.blade.php
+++ resources/views/layout/layouts.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <!-- MDBootstrap -->
     <script src="https://kit.fontawesome.com/825719bb21.js" crossorigin="anonymous"></script>
     <!-- MDB -->
@@ -56,7 +60,7 @@
                 @auth
                 <form action="{{route('logout')}}" method="POST">
                   @csrf
-                  <button type="submit" class="btn btn-link px-3 me-2">LogOuth</button>
+                  <button type="submit" class="btn btn-link px-3 me-2">LogOut</button>
                 </form>
                 @endauth
           
